3650d2a7703d648c0a32e06f7ac0003d6a568c33
Abhishek Pandey <143754400+developerxabhishek@users.noreply.github.com>
2024-03-09 12:43:53 +0530

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/Watchstore").then(()=>{
+mongoose.connect("mongodb+srv://abhishek:abhishek@cluster0.gikozxp.mongodb.net/Watchstore").then(()=>{
     console.log("Sucessfully connected  to database");
 }).catch((err)=>{
     console.log(`Error in connecting ${err}`)
